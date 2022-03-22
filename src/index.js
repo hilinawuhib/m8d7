8659ef6a1fcc68360197326c65140a468813e36d
hilina88 <hilinawuhib@gmail.com>
2022-03-22 15:05:13 +0100

    updated

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { app } from "./app.js";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Connected to Mongo");
+
+  const port = process.env.PORT || 3005;
+
+  app.listen(port, () => {
+    console.log("Server is running on port " + port);
+  });
+});
